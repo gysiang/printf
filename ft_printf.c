8f0241d906ffa42d1e9447400d07d8a2719d6eed
Goh Yong Siang Ivan <gyong-si@c2r1s8.42singapore.sg>
2023-09-27 12:46:55 +0800

    fixed print percentage

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: gyong-si <gyongsi@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:11:08 by gyong-si          #+#    #+#             */
-/*   Updated: 2023/09/26 22:29:24 by gyong-si         ###   ########.fr       */
+/*   Updated: 2023/09/27 12:46:20 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 void ft_putnbr_unsigned(unsigned int n)
 {
@@ -49,6 +50,7 @@ static int ft_voidtohex(void *p)
 		addr /= 16;
 		len++;
 	}
+	ft_putstr_fd("0x", 1);
 	ft_putstr_fd(&hex[i], 1);
 	return (len);
 }
@@ -70,7 +72,7 @@ static int	ft_intohex(unsigned int s, char *base)
 		i--;
 		len++;
 	}
-	ft_putstr_fd(&hex[i], 1);
+	ft_putstr_fd(&hex[i + 1], 1);
 	return (len);
 }
 
@@ -86,44 +88,37 @@ static int	ft_format(va_list args, char format)
 	}
 	else if (format == 's')
 	{
-		char *s;
+		char	*s;
+
 		s = va_arg(args, char *);
 		ft_putstr_fd(s, 1);
 		len += ft_strlen(s);
 	}
 	else if (format == 'p')
-	{
 		len += ft_voidtohex(va_arg(args, void *));
-	}
-	else if (format == 'd')
+	else if (format == 'd' || format == 'i')
 	{
 		int	num;
-		num = va_arg(args, int);
+
+		num = va_arg(args, int);	
 		ft_putnbr_fd(num, 1);
 		if (num < 0)
 			len += ft_numlen(-num) + 1;
 		else
-			len += ft_numlen(num); 
-	}
+			len += ft_numlen(num);
+	} 
 	else if (format == 'u')
 	{
-		unsigned int num;
+		int	num;
+
 		num = (unsigned int)va_arg(args, int);
-		ft_putnbr_unsigned(num);
+		ft_putnbr_unsigned((unsigned int)num);
 		len += ft_numlen(num);
 	}
 	else if (format == 'x')
-	{
-		unsigned int s;
-		s = (unsigned int)va_arg(args, int);
-		len += ft_intohex(s, "0123456789abcdef");
-	}
+		len += ft_intohex((unsigned int)va_arg(args, int), "0123456789abcdef");
 	else if (format == 'X')
-	{
-		unsigned int s;
-		s = (unsigned int)va_arg(args, int);
-		len += ft_intohex(s, "0123456789ABCDEF");
-	}
+		len += ft_intohex((unsigned int)va_arg(args, int), "0123456789ABCDEF");
 	else if (format == '%')
 	{
 		ft_putchar_fd('%', 1);
@@ -136,8 +131,8 @@ static int	ft_format(va_list args, char format)
 
 int	ft_printf(const char *s, ...)
 {
-	int	printed_len;
 	int	i;
+	int	printed_len;
 	va_list	args;
 
 	i = 0;
@@ -158,7 +153,7 @@ int	ft_printf(const char *s, ...)
 		i++;
 	}
 	va_end(args);
-	return (printed_len);
+	return (i);
 }
 
 int	main(void)
@@ -167,20 +162,26 @@ int	main(void)
 	int	num;
 	char *string;
 	unsigned int num1;
-	char *p;
 
 	singlechar = 'h';
 	string = "ivan";
 	num = 42;
 	num1 = 4212345;
-	p = '%';
 	ft_printf("This is a single char %c\n", singlechar);
+	printf("This is a single char %c\n",singlechar);
 	ft_printf("My name is %s. My char is %c\n", string, singlechar);
+	printf("My name is %s. Mr char is %c\n", string, singlechar);
 	ft_printf("Address of num: %p\n", (void *)&num);
+	printf("Address of num: %p\n", (void *)&num);
 	ft_printf("Decimal Base 10 number: %d\n", num);
+	printf("Decimal Base 10 number: %d\n", num);
 	ft_printf("Unsigned Int Decimal Base 10 number: %u\n", num1);
+	printf("Unsigned Int Decimal Base 10 number: %u\n", num1);
 	ft_printf("Print unsigned int number in lowercase hex format: %x\n", num1);
+	printf("Print unsigned int number in lowercase hex format: %x\n", num1);
 	ft_printf("Print unsigned int number in uppercase hex format: %X\n", num1);
-	ft_printf("Prints out percentage sign : %%", p);
+	printf("Print unsigned int number in uppercase hex format: %X\n", num1);
+	ft_printf("Prints out percentage sign : %%\n");
+	printf("Prints out percentage sign : %%\n");
 	return (0);
 }
